b813d0f1cda6a244ab76a6defa4a931f83af207a
Liz Bennett <liz@bennettworld.org>
2013-11-01 22:55:51 -0700

    Added a m4a system dependencies in README.md. Song class is music_crawler.py

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ pyed-pyper
 ==========
 
 A lightweight, web-based music player
+
+DEPENDENCIES
+------------
+-faac, faad2 libraries
--- music_crawler.py
+++ music_crawler.py
@@ -6,6 +6,10 @@ import os
 import sys
 
 
+music_file_types = [".asf", ".flac", ".m4a", ".ape", ".mp3", ".mpc", ".ogg", ".opus", ".ogv", ".oga", ".ogx", ".spx", ".tta", ".wv", and OptimFROG]
+
+songs = []
+
 '''
 crawl_music
 params:
@@ -13,29 +17,31 @@ params:
 '''
 def crawl_music(root):
 
-    music_file_to_path = {}
+    #array of Song objects
     
     if not os.path.isdir(root):
 	fail_and_exit("Cannot scrape from "+root+": not a directory");
 
-    for dirname, dirnames, filenames in os.walk(root):
+    walk_dirs(root)
+
+    return songs
+
+def walk_dirs(dir):
+    global songs
+
+    for dirname, dirnames, filenames in os.walk(dir):
 	# print path to all subdirectories first.
 	for subdirname in dirnames:
-	    print os.path.join(os.path.abspath(dirname), subdirname)
-
-'''
+	    #print "subdirname is "+subdirname
+	    #print os.path.join(os.path.abspath(dirname), subdirname)
+	    walk_dirs(subdirname)
+	
 	# print path to all filenames.
 	for filename in filenames:
+	    #print "filename is "+filename
 	    print os.path.join(os.path.abspath(dirname), filename)
-
-        # Advanced usage:
-	# editing the 'dirnames' list will stop os.walk() from recursing into there.
-        if '.git' in dirnames:
-        # don't go into any .git directories.
-	    dirnames.remove('.git')
-'''
-	
-	return music_file_to_path
+	    if os.path.splitext(filename)[1] in music_file_types:
+		songs.append(make_new_song(os.path.join(os.path.abspath(dirname), filename))
 
 def fail_and_exit(message):
     print message
@@ -45,3 +51,15 @@ def main():
     crawl_music(sys.argv[1])
 
 main()
+
+class Song:
+
+    def __init__(self, path, track_name, artist, song_length):
+	self.path = ""
+	self.track_name = ""
+	self.artist = ""
+	self.song_length = ""
+
+
+
+
